7452621a382a2353c988abf14dbcd20847aae15f
cansucil <cansuucil99@gmail.com>
2023-12-15 22:31:01 +0300

    plugins and router updated

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -17,8 +17,8 @@ export default createVuetify({
     themes: {
       light: {
         colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
+          primary: '#3FBF77',
+          secondary: '#f29b8d'
         },
       },
     },
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,8 +14,40 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import('@/views/Home.vue'),
       },
+      {
+        path: '/factory-details',
+        name: 'FactoryDetails',
+        // route level code-splitting
+        // this generates a separate chunk (Home-[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/FactoryDetail.vue'),
+      },
     ],
   },
+  {
+    path: '/',
+    component: () => import('@/layouts/default/AuthDefault.vue'),
+    children: [
+      {
+        path: '/sign-in',
+        name: 'Sign-in',
+        // route level code-splitting
+        // this generates a separate chunk (Home-[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/Sign-in.vue'),
+      },
+      {
+        path: '/sign-up',
+        name: 'Sign-up',
+        // route level code-splitting
+        // this generates a separate chunk (Home-[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/Sign-up.vue'),
+      },
+    ],
+  },
+
+
 ]
 
 const router = createRouter({
